f06f723bedfc76d9893f82f264b9a9df84656071
GlennRodrigues <glnrod17596@gmail.com>
2018-11-15 14:39:02 +0530

    maven project

---
+++ src/main/java/proj/springApp/App.java
@@ -0,0 +1,21 @@
+package proj.springApp;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Resource resource= new ClassPathResource("applicationContext.xml");
+BeanFactory factory= new XmlBeanFactory(resource);
+Student student=(Student)factory.getBean("myObj");
+student.displayinfo();
+	}
+}
